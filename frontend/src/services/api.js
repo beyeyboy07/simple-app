07b3db918828dce92829c00a9c3e668264d9c101
unknown <beyeyboy07@gmail.com>
2026-09-02 21:39:39 +0700

    fix vercel routing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -28,16 +28,11 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
 
-        // if (error.response?.status === 401) {
-        //     localStorage.removeItem('token')
-        //     localStorage.removeItem('user')
-
-        //     window.location.href = '/login'
-        // }
-
         if (error.response?.status === 401) {
-            console.log('401 ERROR:', error.config?.url)
-            console.log('401 RESPONSE:', error.response?.data)
+            localStorage.removeItem('token')
+            localStorage.removeItem('user')
+
+            window.location.href = '/login'
         }
 
         return Promise.reject(error)
